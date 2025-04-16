e48efd77f3b5a6999a2abfd84edc28204a23ea85
Devika Sivakumar(UST <288033@ust.com>
2025-04-16 21:44:00 +0530

    third commit

--- port/models.py
+++ port/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Custom upload path function
+def item_image_upload_path(instance, filename):
+    # Optional: slugify the title to avoid issues with spaces/special characters
+    from django.utils.text import slugify
+    project_title = slugify(instance.project.title)
+    return f"projects/{project_title}/{filename}"
+
+# Project model
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -9,10 +16,11 @@ class Project(models.Model):
     def __str__(self):
         return self.title
 
+# Item model with custom upload path
 class Item(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='items')
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=item_image_upload_path)
 
     def __str__(self):
         return self.name
--- port/serializers.py
+++ port/serializers.py
@@ -10,4 +10,3 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model=Item
         fields='__all__'
-
