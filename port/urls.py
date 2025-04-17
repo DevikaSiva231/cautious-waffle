2775a846a0b02a3c38a560fd65f7e3f3138e7e45
Devika Sivakumar(UST <288033@ust.com>
2025-04-17 20:53:58 +0530

    itemview and projectview done

--- port/urls.py
+++ port/urls.py
@@ -1,9 +1,16 @@
+from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import ProjectView, ItemView
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('projects/', ProjectView.as_view(), name='project-list'),
+    path('items/', ItemView.as_view(), name='item-list'),
+    path('projects/<int:pk>/', ProjectView.as_view(), name='project-detail'),
+    path('items/<int:pk>/', ItemView.as_view(), name='item-detail'),
+
 ]
 
 if settings.DEBUG:
--- port/views.py
+++ port/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Project, Item
+from .serializers import ProjectSerializer, ItemSerializer
 
 
 # Create your views here.
 class ProjectView(generics.ListCreateAPIView):
-    quesryset=Project.objects.all()
-    serializer_set=ProjectSerializer
+    queryset=Project.objects.all()
+    serializer_class=ProjectSerializer
+
+class ItemView(generics.ListCreateAPIView):
+    queryset=Item.objects.all()
+    serializer_class=ItemSerializer
+
